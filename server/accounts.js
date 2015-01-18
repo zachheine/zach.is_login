8ea3276b65bd3503f4f6ed302e1dbd1b16572b46
Zach Heineman <zach@heineman.info>
2015-01-17 17:04:45 -0800

    Fix service configurations file

--- server/accounts.js
+++ server/accounts.js
@@ -1,29 +1,26 @@
-// first, remove configuration entry in case service is already configured
-Accounts.loginServiceConfiguration.remove({
-  service: "facebook"
-});
-Accounts.loginServiceConfiguration.insert({
-  service: "facebook",
-  appId: "580265375373820",
-  secret: "18b901e941129a6c44cf47df94d8f722"
-});
-
-// first, remove configuration entry in case service is already configured
-Accounts.loginServiceConfiguration.remove({
-  service: "twitter"
-});
-Accounts.loginServiceConfiguration.insert({
-  service: "twitter",
-  consumerKey: "raR9YcCwaj3AzRqMO75b7jfpU",
-  secret: "xxdVl23P94SOfP1eV2TJcXsEs5bZsZHbvZQKemMqnOjMYyFiyd"
+ServiceConfiguration.configurations.update({
+    service:"facebook"
+}, {
+    $set: {
+      appId: "580265375373820",
+      secret: "18b901e941129a6c44cf47df94d8f722"
+    }
 });
 
-// first, remove configuration entry in case service is already configured
-Accounts.loginServiceConfiguration.remove({
-  service: "google"
+ServiceConfiguration.configurations.update({
+    service:"twitter"
+}, {
+    $set: {
+      consumerKey: "raR9YcCwaj3AzRqMO75b7jfpU",
+      secret: "xxdVl23P94SOfP1eV2TJcXsEs5bZsZHbvZQKemMqnOjMYyFiyd"
+    }
 });
-Accounts.loginServiceConfiguration.insert({
-  service: "google",
-  clientId: "552754768826-abc3vqflklmafpji8unsn4r12c3qecc0.apps.googleusercontent.com",
-  secret: "WTskK2uSztinFecSfK2Leoms"
+
+ServiceConfiguration.configurations.update({
+    service:"google"
+}, {
+    $set: {
+      clientId: "552754768826-abc3vqflklmafpji8unsn4r12c3qecc0.apps.googleusercontent.com",
+      secret: "WTskK2uSztinFecSfK2Leoms"
+    }
 });
